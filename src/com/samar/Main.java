13f8f0fb1ffeaf57421db36c8286cd76809999db
samar thakur <sskthakur103@gmail.com>
2020-03-25 12:14:44 +0530

    secon commit

--- src/com/samar/Main.java
+++ src/com/samar/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("my first git project");
+        System.out.println("first commit successful");
     }
 }
